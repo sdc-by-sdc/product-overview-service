c04e27b50458a1484c1f6f5402a64b49a9e66d34
Sarah Cerezo <cerezo.sarah@gmail.com>
2021-10-21 22:57:48 -0400

    git pain issues

--- dotenvSAMPLE.js
+++ dotenvSAMPLE.js
@@ -2,17 +2,6 @@
 
 
 PORT= desired port for server
-URL_BASE= base url; localhost or deployment (includes 'http://"")
-DATABASE_URL= database path (localhost or deployment) + "/productOverview"
-
-PRODUCT= path to product.csv file
-STYLES= path to styles.csv file
-FEATURES= path to features.csv file
-SKUS= path to skus.csv file
-PHOTOS= path to photos.csv file
-RELATED= path to related.csv file
-
-
 
 
 */
