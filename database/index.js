bba5643dab44b8de26b6bd200d31028b73c6a017
Sarah Cerezo <cerezo.sarah@gmail.com>
2021-10-26 00:11:12 -0400

    server side routes functional

--- database/index.js
+++ database/index.js
@@ -1,61 +1,117 @@
-import mongoose from 'mongoose';
-mongoose.connect('mongodb://localhost/productOverview');
-const { Schema } = mongoose;
+const mongoose = require('mongoose');
+const Product = require('./productSchema');
+const Style = require('./styleSchema');
 
-// schema for product documents
-const productSchema = new Schema({
-  id: {
-    type: Number,
-    unique: true
-  },
-  name: String,
-  slogan: String,
-  description: String,
-  category: String,
-  defaultPrice: String,
-  features: [
-    {
-      feature: String,
-      value: String
-    }
-  ],
-  related: [
-    {
-      relatedID: Number
-    }
-  ],
-  styles: [
-    {
-      styleID: Number
-    }
-  ]
-});
+// get products list
+const getProductsList = function(page, count, callback) {
+  const skip = (page - 1) * count;
+  Product.find({}, 'id name slogan description category defaultPrice', { limit: count, skip: skip })
+    .then((results) => {
+      let formatted = [];
+      results.forEach((product) => {
+        const format = {
+          id: product.id,
+          name: product.name,
+          slogan: product.slogan,
+          description: product.description,
+          category: product.category,
+          'default_price': product.defaultPrice
+        };
+        formatted.push(format);
+      });
+      //console.log('DATABASE FORMATED', formatted);
+      callback(null, formatted);
+    })
+    .catch((error) => {
+      callback(error, null);
+    });
+};
 
-// schema for style documents
-const styleSchema = new Schema({
-  styleID: {
-    type: Number,
-    unique: true
-  },
-  name: String,
-  originalPrice: String,
-  salePrice: String,
-  default: Boolean,
-  photos: [
-    {
-      thumbnailURL: String,
-      URL: String
-    }
-  ],
-  skus: [
-    {
-      sku: Number,
-      quantity: Number,
-      size: String
-    }
-  ]
-});
+// get product info
+const getProductInfo = function(productID, callback) {
+  Product.findOne({id: productID})
+    .then((product) => {
+      let formatted = {
+        id: product.id,
+        name: product.name,
+        slogan: product.slogan,
+        description: product.description,
+        category: product.category,
+        'default_price': product.defaultPrice,
+        features: []
+      };
+      product.features.forEach((entry) => {
+        let featureEntry = {
+          feature: entry.feature,
+          value: entry.value
+        };
+        formatted.features.push(featureEntry);
+      });
+      //console.log('DATABASE FORMATED', formatted);
+      callback(null, formatted);
+    })
+    .catch((error) => {
+      callback(error, null);
+    });
+};
 
-// turn schemas into models
-const Product = mongoose.model('Product', productSchema);
-const Style = mongoose.model('Style', styleSchema);
+// get product styles
+const getProductStyles = function(productID, callback) {
+  let finalResult = {
+    'product_id': JSON.stringify(productID),
+    results: []
+  };
+  Product.findOne({id: productID})
+    .then((product) => {
+      const stylesList = product.styles;
+      stylesList.forEach((style) => {
+        Style.findOne({styleID: style.styleID})
+          .then(styleDoc => {
+            let formatted = {
+              'style_id': styleDoc.styleID,
+              name: styleDoc.name,
+              'original_price': styleDoc.originalPrice,
+              'sale_price': styleDoc.salePrice,
+              'default?': styleDoc.default,
+              photos: []
+            };
+            console.log('PHOTO', styleDoc);
+            styleDoc.photos.forEach(photo => {
+              formatted.photos.push({
+                'thumbnail_url': photo.thumbnailURL,
+                url: photo.url
+              });
+            });
+            finalResult.results.push(formatted);
+          })
+          .catch(error => {
+            console.log('ERROR fetching style', error);
+          });
+      });
+      return finalResult;
+    })
+    .then(result => {
+      callback(null, result);
+    })
+    .catch((error) => {
+      callback(error, null);
+    });
+};
+
+// get product related
+const getProductRelated = function(productID, callback) {
+  let relatedIDs = [];
+  Product.findOne({id: productID})
+    .then((product) => {
+      product.related.forEach((id) => {
+        relatedIDs.push(id.relatedID);
+      });
+      //console.log('DATABASE FORMATED', formatted);
+      callback(null, relatedIDs);
+    })
+    .catch((error) => {
+      callback(error, null);
+    });
+};
+
+module.exports = { getProductsList, getProductInfo, getProductStyles, getProductRelated };
